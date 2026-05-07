c792c49a1757ac2625c38660c186835f5627acae
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-05-07 23:13:49 +0530

    Completed auth service with AWS MySQL integration and gateway routing

---
+++ services/auth-service/src/main/java/com/crypto/authservice/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.crypto.authservice.controller;
+
+import com.crypto.authservice.dto.LoginRequest;
+import com.crypto.authservice.dto.RegisterRequest;
+import com.crypto.authservice.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+
+}
